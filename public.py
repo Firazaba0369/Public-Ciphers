547ba58da21a0ffcbe0f950397a70cd05c83cbca
Firazaba0369 <123702516+Firazaba0369@users.noreply.github.com>
2025-01-23 23:41:21 -0800

    added python interpreter and setup main function as well as diffie hellman function

---
+++ public.py
@@ -0,0 +1,15 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+
+def main():
+    # Add your code here
+    pass
+
+#
+def diffie_hellman():
+    # Add your code here
+    pass
+
+if __name__ == '__main__':
+    main()
